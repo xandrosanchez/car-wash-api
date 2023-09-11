ce64a531d975dee4575d8a43d3e1c71163b1acb1
xandrosanchez <din.myrk@gmail.com>
2023-09-11 22:45:35 +0300

    Initial commit

---
+++ src/main/java/com/example/carwashapi/CarWashApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.carwashapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarWashApiApplication.class, args);
+	}
+
+}
